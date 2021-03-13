8eefa3ca40a80f0eb490de0fa02d88e445ea412d
Daniel Villamizar <danvilla@stanford.edu>
2021-03-12 16:49:00 -0800

    Update settings/reqs and make scripts/outputs

--- program_chip.py
+++ program_chip.py
@@ -6,9 +6,10 @@ from nirram import NIRRAM
 parser = argparse.ArgumentParser(description="Program a bitstream to a chip.")
 parser.add_argument("chipname", help="chip name for logging")
 parser.add_argument("bitstream", help="bitstream file name")
-parser.add_argument("--start-addr", type=int, default=0, action="store_const", help="start addr")
-parser.add_argument("--end-addr", type=int, default=65536, action="store_const", help="end addr")
-parser.add_argument("--step-addr", type=int, default=1, action="store_const", help="addr step")
+parser.add_argument("--start-addr", type=int, default=0, help="start addr")
+parser.add_argument("--end-addr", type=int, default=65536, help="end addr")
+parser.add_argument("--step-addr", type=int, default=1, help="addr step")
+parser.add_argument("--iterations", type=int, default=3, help="number of programming iterations")
 args = parser.parse_args()
 
 # Initialize NI system
@@ -18,14 +19,15 @@ nisys = NIRRAM(args.chipname)
 bitstream = open(args.bitstream).readlines()
 
 # Do operation across cells
-for addr, bit in zip(range(args.start_addr, args.end_addr, args.step_addr), bitstream):
-    nisys.set_addr(addr)
-    bit = int(bit.strip())
-    if bit == 0:
-        target = nisys.target(9e3, 11e3)
-    if bit == 1:
-        target = nisys.target(100e3, 1e9)
-    print(f"Address {addr}: {target}")
+for i in range(args.iterations):
+    for addr, bit in zip(range(args.start_addr, args.end_addr, args.step_addr), bitstream):
+        nisys.set_addr(addr)
+        bit = int(bit.strip())
+        if bit == 0:
+            target = nisys.target(9e3, 11e3)
+        if bit == 1:
+            target = nisys.target(100e3, 1e9)
+        print(f"Iteration {i}, Address {addr}: {target}")
 
 # Shutdown
 nisys.close()
---
+++ read_chip.py
@@ -0,0 +1,29 @@
+"""Script to FORM a chip"""
+import argparse
+from nirram import NIRRAM
+
+# Get arguments
+parser = argparse.ArgumentParser(description="FORM a chip.")
+parser.add_argument("chipname", help="chip name for logging")
+parser.add_argument("outfile", help="file to output to")
+parser.add_argument("--start-addr", type=int, default=0, help="start address")
+parser.add_argument("--end-addr", type=int, default=65536, help="end address")
+parser.add_argument("--step-addr", type=int, default=1, help="address stride")
+args = parser.parse_args()
+
+# Open outfile
+outfile = open(args.outfile, "a")
+
+# Initialize NI system
+nisys = NIRRAM(args.chipname)
+
+# Do operation across cells
+for addr in range(args.start_addr, args.end_addr, args.step_addr):
+    nisys.set_addr(addr)
+    read = nisys.read()
+    outfile.write(f"{addr}\t{read[0]}\n")
+    print(f"{addr}\t{read[0]}")
+
+# Shutdown
+outfile.close()
+nisys.close()
