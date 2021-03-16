7668e3f1e5f27fac9e4d8790bd5627f865a18c56
Daniel Villamizar <danvilla@stanford.edu>
2021-03-15 17:39:19 -0700

    add LRS/HRS args to program_chip

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Install: hightime, numpy, nitclk, nifgen, nidcpower, nidaqmx
 - program_chip.py: Program a bitstream to a chip
 
 Make sure to specify the chip name in the arguments when running either script. Program chip also needs the bitstream file. Two examples can be found in `bitstream/` and the script for generating more can also be found in the other repo.
+
+
+
+## Change notes (haitong)
+- Add args for target LRS/HRS windows when programming a chip
+- Could be perhaps moved into self.settings and then specify a different setting json when targetting at different resistance levels
+    - if SET/RESET pulse settings also need to be modified together, this approach would be more convenient 
--- program_chip.py
+++ program_chip.py
@@ -6,6 +6,11 @@ from nirram import NIRRAM
 parser = argparse.ArgumentParser(description="Program a bitstream to a chip.")
 parser.add_argument("chipname", help="chip name for logging")
 parser.add_argument("bitstream", help="bitstream file name")
+
+# Expect to receive two arg numbers when specifying a LRS (or HRS) range
+parser.add_argument("--lrs-range", nargs='+', type=float, default=[9e3, 11e3], help="target LRS")
+parser.add_argument("--hrs-range", nargs='+', type=float, default=[100e3, 1e9], help="target HRS")
+
 parser.add_argument("--start-addr", type=int, default=0, help="start addr")
 parser.add_argument("--end-addr", type=int, default=65536, help="end addr")
 parser.add_argument("--step-addr", type=int, default=1, help="addr step")
@@ -23,10 +28,10 @@ for i in range(args.iterations):
     for addr, bit in zip(range(args.start_addr, args.end_addr, args.step_addr), bitstream):
         nisys.set_addr(addr)
         bit = int(bit.strip())
-        if bit == 0:
-            target = nisys.target(9e3, 11e3)
-        if bit == 1:
-            target = nisys.target(100e3, 1e9)
+        if bit == 0:  # bit 0: LRS
+            target = nisys.target(args.lrs_range[0], args.lrs_range[1])
+        if bit == 1:  # bit 1: HRS
+            target = nisys.target(args.hrs_range[0], args.hrs_range[1])
         print(f"Iteration {i}, Address {addr}: {target}")
 
 # Shutdown
